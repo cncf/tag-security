affb4df65436c5a6acd3a02cd0bac4ae8e1dd865
John Hillegass <JohnHillegass@users.noreply.github.com>
2021-01-22 21:18:19 +0000

    move config files to ci directory

--- ci/links.sh
+++ ci/links.sh
@@ -4,6 +4,6 @@ npm install -g markdown-link-check
 git fetch origin master:master
 for file_name in $(git diff --name-only $HEAD master); do
   if [[ $file_name == *".md" ]]; then
-    npx markdown-link-check --config link-check-config.json --progress --verbose "$file_name"
+    npx markdown-link-check --config ./ci/link-check-config.json --progress --verbose "$file_name"
   fi
 done
