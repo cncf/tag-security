241bc2e076af28170a9c6cbd49c5adb133383d90
Martin Tverråen <martin.tverraen@evry.com>
2021-03-31 21:09:22 +0200

    landing page info hack

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from "react";
 import { useState } from 'react';
 import Head from "next/head";
+import styles from '../styles/Home.module.css'
 import {parseIndexFileToTree} from '../components/topicTree'
+import matter from 'gray-matter'
+import ReactMarkdown from 'react-markdown'
 
-const Index = ({ data, topicTree }) => {
+const Index = ({ data, topicTree, landingPage}) => {
   return (
     <div>
       <Head>
@@ -11,26 +14,10 @@ const Index = ({ data, topicTree }) => {
         <meta charSet="utf-8" />
         <title>CNSmap</title>
       </Head>
-      <div>
-       <h2>CNCF Cloud Native Security Map</h2>
-       <p>The Cloud Native Security Map aims to: </p>
-         <ul>
-           <li>
-           provide a mapping of CNCF and open source projects to areas of CN Security whitepaper
-           </li>
-           <li>
-           Identify gaps in CN Security in the ecosystem and make recommendations to TOC
-           </li>
-           <li>
-           Help educate practitioners of what technologies can be used in practice and how they tie into each other
-           </li>
-           <li>
-           Provide practical tips or examples for how to use tools within this category, or why they are important (I.e. example breaches, etc.)
-           </li>
-           <li>
-           Provide a reference for frameworks to utilize when developing CN Security solutions and architectures.
-           </li>
-         </ul>
+      <div className={styles.main_content}>
+      <h1>Cloud Native Security Map </h1>
+      <h2>A practitioner's and learner's guide on how to navigate the Cloud Native security landscape</h2>
+      <p>The Cloud Native Security map builds on top of the Cloud Native Security Whitepaper by SIG-Security, by providing an additional practitioner's perspective as well as an interactive mode of consumption, to facilitate the exploration of Cloud Native Security concepts and how they are used. The Cloud Native Security Map provides mapping of security topics to projects that one can use, as well as provide some examples of using these projects to help illustrate the controls and configuration required.</p>
       </div>
     </div>
   );
@@ -45,13 +32,19 @@ export async function getStaticProps() {
 
   const indexList = fs.readFileSync(`${process.cwd()}/content/index`, "utf-8");
   const topicTree = parseIndexFileToTree(indexList);
+
+  const landingPage =fs.readFileSync(`${process.cwd()}/content/landing.md`, "utf-8");
+
   // Use topic tree here for content later on
 
   /*
   const files = fs.readdirSync(`${process.cwd()}/content`, "utf-8");
 
   const blogs = files.filter((fn) => fn.endsWith(".md"));
-
+  // Import our .md file using the `slug` from the URL
+  const content = await import(`../content/${slug}.md`)
+  // Parse .md data through `matter`
+  const data = matter(content.default)
   // const data = matter(content.default);
   const data = blogs.map((blog) => {
     const path = `${process.cwd()}/content/${blog}`;
@@ -65,6 +58,7 @@ export async function getStaticProps() {
   return {
     props: {
       topicTree: JSON.parse(JSON.stringify(topicTree)),
+      landingPage:landingPage,
     },
   };
 }
