da6dbecfcc927628da0b6549717350c89b5435d1
Brandon Lum <lumjjb@gmail.com>
2021-03-31 16:07:06 -0400

    change header type for main page
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,8 @@ const Index = ({ data, topicTree, landingPage}) => {
       </Head>
       <div className={styles.main_content}>
       <h1>Cloud Native Security Map </h1>
-      <h2>A practitioner's and learner's guide on how to navigate the Cloud Native security landscape</h2>
+      <h4>A practitioner's and learner's guide on navigating the Cloud Native security landscape</h4>
+      <br/>
       <p>The Cloud Native Security map builds on top of the Cloud Native Security Whitepaper by SIG-Security, by providing an additional practitioner's perspective as well as an interactive mode of consumption, to facilitate the exploration of Cloud Native Security concepts and how they are used. The Cloud Native Security Map provides mapping of security topics to projects that one can use, as well as provide some examples of using these projects to help illustrate the controls and configuration required.</p>
       </div>
     </div>
