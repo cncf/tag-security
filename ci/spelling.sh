fbd725cee60fcbf224d627d367ed788902408485
Brandon Lum <lumjjb@gmail.com>
2021-06-20 11:13:13 -0400

    Fix bug in spelling.sh
    @JohnHillegass can you verify that this is right? If not it seems like spell check stalls on xargs waiting on STDIN ?

--- ci/spelling.sh
+++ ci/spelling.sh
@@ -2,4 +2,4 @@
 
 npm install -g cspell
 git fetch origin main:main
-git diff --name-only main $HEAD | grep .md | grep . && xargs -L1 npx cspell -c ./ci/spelling-config.json || echo "no markdown files changed"
+git diff --name-only main $HEAD | grep .md | grep . | xargs -L1 npx cspell -c ./ci/spelling-config.json || echo "no markdown files changed"
