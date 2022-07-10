2c657364049388f312809d223a3e34bc85f0adb4
Liran Tal <liran.tal@gmail.com>
2022-07-10 21:37:52 +0300

    build: show suggestions (#937)
    to make the CI helpful and allow folks to easily and quickly fix their spellings I enabled the --show-suggestions CLI options 
    
    Signed-off-by: Liran Tal <liran.tal@gmail.com>
    
    Co-authored-by: Brandon Lum <lumjjb@gmail.com>

--- ci/spelling.sh
+++ ci/spelling.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 npm install -g cspell
 git fetch origin main:main
-git diff --name-only --diff-filter=AM main $HEAD | grep '\.md$' | xargs --no-run-if-empty -L1 npx cspell -c ./ci/spelling-config.json
+git diff --name-only --diff-filter=AM main $HEAD | grep '\.md$' | xargs --no-run-if-empty -L1 npx cspell --show-suggestions -c ./ci/spelling-config.json
