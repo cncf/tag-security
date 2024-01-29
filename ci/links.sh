e01a63412c17879c4b00bbd501dcab5110642ecd
Tuomo Tanskanen <tuomo@tanskanen.org>
2024-01-29 21:58:05 +0200

    ci: fix ci linter scripts (#1223)
    Signed-off-by: Mariusz Sabath <mrsabath@gmail.com>

--- ci/links.sh
+++ ci/links.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+shopt -s globstar
+
+FAILURE=0
+
+git config --global --add safe.directory /usr/src/app
 npm install -g markdown-link-check
 git fetch origin main:main
 # To run this on the entire repo, replace the following command with `$(find ./ -type f | grep .md)`
-for file_name in $(git diff --name-only $HEAD main); do
-  if [[ $file_name == *".md" ]]; then
-    npx markdown-link-check --config ./ci/link-config.json --progress --verbose "$file_name"
-  fi
+for file_name in $(git diff --name-only $HEAD main -- ./**/*.md); do
+  npx markdown-link-check --config ./ci/link-config.json --progress --verbose "$file_name" || FAILURE=1
 done
+
+exit $FAILURE
--- ci/lint.sh
+++ ci/lint.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+set -e
+shopt -s globstar
+
+FAILURE=0
+
+git config --global --add safe.directory /usr/src/app
 npm install -g markdownlint-cli
 git fetch origin main:main
 # To run this on the entire repo, replace the following command with `$(find ./ -type f | grep .md)`
-for file_name in $(git diff --name-only $HEAD main); do
-  if [[ $file_name == *".md" ]]; then
-    markdownlint -c ./ci/lint-config.json "$file_name"
-  fi
+for file_name in $(git diff --name-only $HEAD main -- ./**/*.md); do
+  markdownlint -c ./ci/lint-config.json "$file_name" || FAILURE=1
 done
+
+exit $FAILURE
