917ef6032c6281516ab37af1a462a0c377bbfb98
Andrés Vega <av@messier42.com>
2024-06-20 00:25:51 -0700

    Proper syntax in setup.sh

--- ci/setup.sh
+++ ci/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git config --global --add safe.directory /usr/src/app
+git config --global --add safe.directory "/usr/src/app"
 git fetch origin main:main
 git diff --name-only main $HEAD > modified
 echo "these files have changed"
